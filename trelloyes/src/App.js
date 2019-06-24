64c088ef9cb6c14ebf785ae2899da58138dba442
Whitney Sam <whitneysamwallace@gmail.com>
2019-06-24 12:28:46 -0700

    updated trelloyes app

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -1,19 +1,54 @@
 import React from 'react';
-import './App.css';
-import Card from './Card';
 import List from './List';
+import './App.css';
 
-function App(store) {
+
+function App(props) {
+
+  const lists = props.store.lists.map(list => <List key={list.id} header={list.header} cards={list.cardIds.map(id => props.store.allCards[id])} />)
+
+
   return (
-    <main class="App">
-    <header class="App-header">
+    <main className="App">
+    <header className="App-header">
       <h1>Trelloyes!</h1>
     </header>
-    <div class="App-list">
-      {List}
+    <div className="App-list">
+      {lists}
     </div>
     </main>
   );
 }
 
 export default App;
+
+// class App extends Component {
+//   static defaultProps = {
+//     store: {
+//       lists: [],
+//       allCards: {},
+//     }
+//   };
+
+//   render() {
+//     const { store } = this.props
+//     return (
+//       <main className='App'>
+//         <header className='App-header'>
+//           <h1>Trelloyes!</h1>
+//         </header>
+//         <div className='App-list'>
+//           {store.lists.map(list => (
+//             <List
+//               key={list.id}
+//               header={list.header}
+//               cards={list.cardIds.map(id => store.allCards[id])}
+//             />
+//           ))}
+//         </div>
+//       </main>
+//     );
+//   }
+// }
+
+// export default App;
--- trelloyes/src/List.js
+++ trelloyes/src/List.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import Card from './Card';
-import STORE from './store';
+import './List.css';
 
 function List(props){
 
-    // const cards = [
-
-    //     {
-    //     title,
-    //     content,
-    //     },
-
-    // ];
-
-    const cardList = STORE.lists.map(card => <Card key={card.id} title={card.title} content = {card.content} />) 
+    const cardList = props.cards.map(card => <Card key={card.id} title={card.title} content={card.content} />) 
 
     return (
         <section className="List">
         <header className="List-header">
           <h2>{props.header}</h2>
         </header>
-        <div class="List-cards">
+        <div className="List-cards">
             {cardList}
+            <button type="button" className="List-add-button">
+              + Add Random Card
+            </button>
         </div>
         </section>
     )
